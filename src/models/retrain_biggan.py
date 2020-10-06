a7e7246047842b7228669081236faf1abc9ab5aa
Alex <alexsaadfalcon@yahoo.com>
2020-10-06 14:01:56 -0400

    Made new file for trimmed generator architecture

--- src/models/retrain_biggan.py
+++ src/models/retrain_biggan.py
@@ -32,6 +32,9 @@ except:
     from datasets import ImageFolder
     from sync_batchnorm import patch_replication_callback
 
+
+import src.models.trimmed_biggan as trimmed_biggan
+
 pretrained_folder = 'models/pytorch_biggan/pretrained/100k/'
 CONFIG = {
     'dataset': 'met',
@@ -72,11 +75,11 @@ CONFIG = {
 }
 
 
-def test_image(G, config):
+def test_pretrain_sample(G, config):
     print('creating test image')
     gen_batch = 1
     # z_, y_ = utils.prepare_z_y(gen_batch, config['dim_z'], config['n_classes'])
-    utils.seed_rng(11234)
+    # utils.seed_rng(0)
     z_, y_ = utils.prepare_z_y(gen_batch, config['dim_z'], 1000)
     z_.sample_()
     y_.sample_()
@@ -88,6 +91,10 @@ def test_image(G, config):
     image = np.transpose(image[0], (1, 2, 0))
     plt.imshow(image)
     plt.show()
+
+
+def sample_gen(G, config):
+    pass
 
 
 # The main training file. Config is a dictionary specifying the configuration
@@ -125,8 +132,8 @@ def run(config):
     print('Experiment name is %s' % experiment_name)
 
     # Next, build the model
-    G = model.Generator(**config).to(device)
-    D = model.Discriminator(**config).to(device)
+    G = trimmed_biggan.Generator(**config).to(device)
+    # D = model.Discriminator(**config).to(device)
 
     # remove dimensions that will differ
     for blocklist in G.blocks:
@@ -152,15 +159,10 @@ def run(config):
         G = G.half()
         if config['ema']:
             G_ema = G_ema.half()
-    if config['D_fp16']:
-        print('Casting D to fp16...')
-        D = D.half()
-        # Consider automatically reducing SN_eps?
-    GD = model.G_D(G, D)
+
     # print(G)
-    # print(D)
-    print('Number of params in G: {} D: {}'.format(
-        *[sum([p.data.nelement() for p in net.parameters()]) for net in [G, D]]))
+    print('Number of params in G: {}'.format(
+        *[sum([p.data.nelement() for p in net.parameters()]) for net in [G, ]]))
     # Prepare state dict, which holds things like epoch # and itr #
     state_dict = {'itr': 0, 'epoch': 0, 'save_num': 0, 'save_best_num': 0,
                   'best_IS': 0, 'best_FID': 999999, 'config': config}
@@ -176,121 +178,21 @@ def run(config):
         state_dict['itr'] = 0
         state_dict['epoch'] = 0
 
-    # If parallel, parallelize the GD module
-    if config['parallel']:
-        GD = nn.DataParallel(GD)
-        if config['cross_replica']:
-            patch_replication_callback(GD)
 
-    test_image(G, config)
+    # can only do this if we remove 'del G.shared' above
+    # test_pretrain_sample(G, config)
+    config['input_shape'] = (128, 128)
 
-    # Prepare loggers for stats; metrics holds test metrics,
-    # lmetrics holds any desired training metrics.
-    test_metrics_fname = '%s/%s_log.jsonl' % (config['logs_root'],
-                                              experiment_name)
-    train_metrics_fname = '%s/%s' % (config['logs_root'], experiment_name)
-    print('Inception Metrics will be saved to {}'.format(test_metrics_fname))
-    test_log = utils.MetricsLogger(test_metrics_fname,
-                                   reinitialize=(not config['resume']))
-    print('Training Metrics will be saved to {}'.format(train_metrics_fname))
-    train_log = utils.MyLogger(train_metrics_fname,
-                               reinitialize=(not config['resume']),
-                               logstyle=config['logstyle'])
-    # Write metadata
-    utils.write_metadata(config['logs_root'], experiment_name, config, state_dict)
-    # Prepare data; the Discriminator's batch size is all that needs to be passed
-    # to the dataloader, as G doesn't require dataloading.
-    # Note that at every loader iteration we pass in enough data to complete
-    # a full D iteration (regardless of number of D steps and accumulations)
-    D_batch_size = (config['batch_size'] * config['num_D_steps']
-                    * config['num_D_accumulations'])
-    loaders = utils.get_data_loaders(**{**config, 'batch_size': D_batch_size,
-                                        'start_itr': state_dict['itr']})
+    x = np.random.random(config['input_shape'])
+    y = G(x)
 
-    # Prepare inception metrics: FID and IS
-    get_inception_metrics = inception_utils.prepare_inception_metrics(config['dataset'], config['parallel'],
-                                                                      config['no_fid'])
+    def img_gen():
+        for f in os.listdir('data/processed/'):
+            print('data file', f)
 
-    # Prepare noise and randomly sampled label arrays
-    # Allow for different batch sizes in G
-    G_batch_size = max(config['G_batch_size'], config['batch_size'])
-    z_, y_ = utils.prepare_z_y(G_batch_size, G.dim_z, config['n_classes'],
-                               device=device, fp16=config['G_fp16'])
-    # Prepare a fixed z & y to see individual sample evolution throghout training
-    fixed_z, fixed_y = utils.prepare_z_y(G_batch_size, G.dim_z,
-                                         config['n_classes'], device=device,
-                                         fp16=config['G_fp16'])
-    fixed_z.sample_()
-    fixed_y.sample_()
-
-    # TODO: TRAINING FUNCTION WILL DEFINITELY CHANGE
-    # Loaders are loaded, prepare the training function
-    if config['which_train_fn'] == 'GAN':
-        train = train_fns.GAN_training_function(G, D, GD, z_, y_,
-                                                ema, state_dict, config)
-    # Else, assume debugging and use the dummy train fn
-    else:
-        train = train_fns.dummy_training_function()
-    # Prepare Sample function for use with inception metrics
-    sample = functools.partial(utils.sample,
-                               G=(G_ema if config['ema'] and config['use_ema']
-                                  else G),
-                               z_=z_, y_=y_, config=config)
-
-    print('Beginning training at epoch %d...' % state_dict['epoch'])
-    # Train for specified number of epochs, although we mostly track G iterations.
-    for epoch in range(state_dict['epoch'], config['num_epochs']):
-        # Which progressbar to use? TQDM or my own?
-        if config['pbar'] == 'mine':
-            pbar = utils.progress(loaders[0], displaytype='s1k' if config['use_multiepoch_sampler'] else 'eta')
-        else:
-            pbar = tqdm(loaders[0])
-        for i, (x, y) in enumerate(pbar):
-            # Increment the iteration counter
-            state_dict['itr'] += 1
-            # Make sure G and D are in training mode, just in case they got set to eval
-            # For D, which typically doesn't have BN, this shouldn't matter much.
-            G.train()
-            D.train()
-            if config['ema']:
-                G_ema.train()
-            if config['D_fp16']:
-                x, y = x.to(device).half(), y.to(device)
-            else:
-                x, y = x.to(device), y.to(device)
-            metrics = train(x, y)
-            train_log.log(itr=int(state_dict['itr']), **metrics)
-
-            # Every sv_log_interval, log singular values
-            if (config['sv_log_interval'] > 0) and (not (state_dict['itr'] % config['sv_log_interval'])):
-                train_log.log(itr=int(state_dict['itr']),
-                              **{**utils.get_SVs(G, 'G'), **utils.get_SVs(D, 'D')})
-
-            # If using my progbar, print metrics.
-            if config['pbar'] == 'mine':
-                print(', '.join(['itr: %d' % state_dict['itr']]
-                                + ['%s : %+4.3f' % (key, metrics[key])
-                                   for key in metrics]), end=' ')
-
-            # Save weights and copies as configured at specified interval
-            if not (state_dict['itr'] % config['save_every']):
-                if config['G_eval_mode']:
-                    print('Switchin G to eval mode...')
-                    G.eval()
-                    if config['ema']:
-                        G_ema.eval()
-                train_fns.save_and_sample(G, D, G_ema, z_, y_, fixed_z, fixed_y,
-                                          state_dict, config, experiment_name)
-
-            # Test every specified interval
-            if not (state_dict['itr'] % config['test_every']):
-                if config['G_eval_mode']:
-                    print('Switchin G to eval mode...')
-                    G.eval()
-                train_fns.test(G, D, G_ema, z_, y_, state_dict, config, sample,
-                               get_inception_metrics, experiment_name, test_log)
-        # Increment epoch counter at end of epoch
-        state_dict['epoch'] += 1
+    img_gen()
+    def train_step(x, y):
+        G.optim.zero_grad()
 
 
 def main():
