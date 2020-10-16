d2ab83b4a922eceece0a9cad551c550858ce2d7e
Alex <alexsaadfalcon@yahoo.com>
2020-10-16 12:59:53 -0400

    Fixed test script

--- src/models/test_vasegen_pix2pix.sh
+++ src/models/test_vasegen_pix2pix.sh
@@ -1,2 +1,25 @@
 set -ex
-python test.py --dataroot ../../data/processed/pix2pix_vase_examples_512 --name downstairs --model pix2pix --netG unet_512 --direction BtoA --dataset_mode aligned --norm batch
+cd models/pix2pix
+
+size=512
+
+if [ $size == '256' ]; then
+
+model=unet_256
+batch=16
+
+elif [ $size == '512' ]; then
+
+model=unet_512
+batch=8
+
+else
+
+echo unknown size $size
+
+fi
+
+python test.py \
+--dataroot ../../data/processed/pix2pix_vase_examples_512 --name pix2pix_vase_fragments_$size \
+--model pix2pix --netG $model --direction BtoA --dataset_mode aligned --norm batch \
+--eval --preprocess none
