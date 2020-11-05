239163041e97ff3b0f44a398933eb898c6e7bcfe
Alex <alexsaadfalcon@yahoo.com>
2020-11-04 21:33:38 -0500

    Added S3 commands

--- setup_aws.sh
+++ setup_aws.sh
@@ -3,6 +3,11 @@ sudo apt-get install python3 python3-pip
 sudo ln -s /bin/python3 /bin/python
 pip3 install --no-cache-dir -r requirements_aws.txt
 
+pthdir=models/pix2pix/checkpoints/pix2pix_vase_fragments_512/
+mkdir -p $pthdir
+sudo apt-get install awscli
+sudo aws s3 cp s3://vasegen-matched-512/latest_net_G.pth $pthdir
+
 sudo apt-get install apache2 libapache2-mod-wsgi
 
 sudo ln -sT src/web /var/www/html/flaskapp
@@ -12,3 +17,5 @@ echo "Add the following to"
 echo "/etc/apache2/sites-enabled/000-default.conf"
 cat src/web/mod_wsgi.txt
 
+echo
+echo "Then call \"sudo service apache2 restart\""
